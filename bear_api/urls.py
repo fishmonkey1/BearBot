394224119a3e6e69c2289b39da97cb05c7b83fdf
Jose Epifanio <jose.epifanio90@gmail.com>
2019-08-16 18:16:52 -0500

    Change the url we previously had

--- bear_api/urls.py
+++ bear_api/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url, include
+
 from rest_framework.routers import DefaultRouter
+
 from bear_api import views
 
-# router = DefaultRouter()
-# router.register(r'questions', views.QuestionsViewSet)
-
 urlpatterns = [
-    url(r'^user/(?P<account>.+)?$', views.Users.as_view()),
-    url(r'^reportg/<str:account>?$', views.Users.as_view()),
+    url(r'^user/(?P<account>.+)?$', views.lookup_user),
+    url(r'^report/(?P<id>.+)?$', views.lookup_report),
+    url(r'^analysis?$', views.generate_report),
 ]
