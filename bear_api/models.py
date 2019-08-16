1af5b93c461e032be270312989925e9ae9381d08
Jose Epifanio <jose.epifanio90@gmail.com>
2019-08-16 18:16:10 -0500

    Add the report model

--- bear_api/models.py
+++ bear_api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Report(models.Model):
+    user = models.CharField(max_length=50)
+    tweets = models.IntegerField()
+    followers = models.IntegerField()
+    following = models.IntegerField()
+    verified = models.BooleanField()
+    img = models.TextField(default='https://pbs.twimg.com/profile_images/661318201473544192/QYYw4uBv.png')  # nopep8
+    description = models.TextField(default=' ')
+    url = models.TextField(default='https://twitter.com/home')
+    analysis_positive = models.IntegerField()
+    analysis_negative = models.IntegerField()
+    analysis_neutral = models.IntegerField()
+    acceptance_positive = models.IntegerField()
+    acceptance_neutral = models.IntegerField()
+    acceptance_negative = models.IntegerField()
+    created_on = models.DateField(auto_now_add=True)
